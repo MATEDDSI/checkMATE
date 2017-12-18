9f0b6c18f4cdc95ce16b5472549ee46640c9fa40
InakiMadi <inakimadi@gmail.com>
2017-12-18 01:19:42 +0100

    Implementacion completa de Consulta

--- checkMATE.rb
+++ checkMATE.rb
@@ -6,6 +6,10 @@ checkMATE.
 Usage:
 	#{__FILE__} consejo contra <jugador> <juego>
 	#{__FILE__} consulta <juego>
+	#{__FILE__} consulta <juego> empate
+	#{__FILE__} consulta <juego> diferencia <numero>
+	#{__FILE__} consulta <juego> restriccion <atributo> <valor>
+	#{__FILE__} consulta ID <numero>
 	
 Options:
 	-h --help	Muestra esta pantalla.
@@ -17,16 +21,27 @@ DOCOPT
 begin
 	require "docopt"
 	args = Docopt::docopt(doc)
+
 	if args["consejo"]
 		require_relative "consejos"
 		Consejos.maneja_argumentos(args);
 	end
+
 	if args["consulta"]
 		require_relative "consulta"
-		Consulta.consultar(args["<juego>"]);
-#		Consulta.consultarCondicion(args["<juego>"],args["<diferencia/igual>"],args["<numero>"]);
-#		Consulta.restriccion(args["<juego>"],args["<atributo>"],args["<valor>"])
+		if args["empate"]
+			Consulta.consultarCondicion(args["<juego>"],'diferencia',0);
+		elsif args["diferencia"]
+			Consulta.consultarCondicion(args["<juego>"],args["diferencia"],args["<numero>"]);
+		elsif args["restriccion"]
+			Consulta.restriccion(args["<juego>"],args["<atributo>"],args["<valor>"])
+		elsif args["ID"]
+			Consulta.consultarID(args["<numero>"]);
+		else
+			Consulta.consultar(args["<juego>"]);
+		end
 	end
+
 rescue Docopt::Exit => e
 	puts e.message
 end
--- consulta.rb
+++ consulta.rb
@@ -9,62 +9,69 @@ module Consulta
 
 
 	def Consulta.consultarCondicion(juego,condicion,numero)
-		if condicion == 'diferencia'
-			res_id = {}
-			id_list = sacarIDs(juego)
-			id_list.each do |id_i|
-				nomypun_list = sacarNombreJugsYPuntuaciones(id_i["idPartida"])
-				p_a = nomypun_list["puntuacion"][0]
-				p_b = nomypun_list["puntuacion"][1]
-				if( (p_a - p_b)*(p_a - p_b) < numero*numero)
+		# condicion == diferencia
+		res_id = []
+		id_list = sacarIDs(juego)
+		id_list.each do |id_i|
+			nomypun_list = sacarNombreJugsYPuntuaciones(id_i["idPartida"])
+			if nomypun_list.length == 2
+				p_a = nomypun_list[0]["puntuacion"].to_i
+				p_b = nomypun_list[1]["puntuacion"].to_i
+				if( (p_a - p_b)*(p_a - p_b) <= numero.to_i*numero.to_i)
 					res_id << id_i
 				end
 			end
-
-			res_id.each do |id_i|
-				mostrarID(id_i)
-			end
-		else
-			consultarCondicion(juego,'diferencia',0)
+		end
+		res_id.each do |id_j|
+			mostrarID(id_j)
 		end
 	end
 
-	def Consulta.mostrarID(id_i)
-		puts "Partida #{id_i["idPartida"]}:"
+	def Consulta.consultarID(numero)
+		id = { "idPartida" => numero.to_i }
+		mostrarID(id)
+	end
 
-		nomypun_list = sacarNombreJugsYPuntuaciones(id_i["idPartida"])
-		print "   " , "Jugadores: "
-		cont = 0
-		nomypun_list.each do |nomypun_i|
-			if nomypun_list.length == cont
-				print nomypun_i["nombreJugador"]
-			else
-				print nomypun_i["nombreJugador"] , "vs. "
+	def Consulta.mostrarID(id)
+		nomypun_list = sacarNombreJugsYPuntuaciones(id["idPartida"])
+		if( nomypun_list.empty? )
+			puts "No existe la Partida #{id["idPartida"]}."
+		else
+			puts "--Partida #{id["idPartida"]}--"
+			print "   " , "Jugadores: "
+			cont = 1
+			nomypun_list.each do |nomypun_i|
+				if nomypun_list.length == cont
+					print nomypun_i["nombreJugador"]
+				else
+					print nomypun_i["nombreJugador"] , " vs. "
+				end
+				cont = cont +1
 			end
-			cont = cont +1
-		end
-		print "\n"
+			print "\n"
 
-		print "   " , "Puntuaciones: "
-		cont = 0
-		nomypun_list.each do |nomypun_i|
-			if nomypun_list.length == cont
-				print nomypun_i["puntuacion"]
-			else
-				print nomypun_i["puntuacion"] , "-"
+			print "   " , "Resultado: "
+			cont = 1
+			nomypun_list.each do |nomypun_i|
+				if nomypun_list.length == cont
+					print nomypun_i["puntuacion"]
+				else
+					print nomypun_i["puntuacion"] , "-"
+				end
+				cont = cont +1
 			end
-			cont = cont +1
-		end
-		print "\n"
+			print "\n"
 
 		
-		nomypun_list.each do |nomypun_i|
-			puts "     " + nomypun_i["nombreJugador"]
-			atrval_list = sacarAtributosValores(id_i["idPartida"],nomypun_i["nombreJugador"])
-			atrval_list.each do |atrval_i|
-				puts "     " , atrval_i["nombreAtributo"] , " -> " , atrval_i["valor"]
+			nomypun_list.each do |nomypun_i|
+				puts "     " + nomypun_i["nombreJugador"] + ":"
+				atrval_list = sacarAtributosValores(id["idPartida"],nomypun_i["nombreJugador"])
+				atrval_list.each do |atrval_i|
+					puts "       " + atrval_i["nombreAtributo"] + " -> " + atrval_i["valor"]
+				end
 			end
 		end
+		
 	end
 
 	def Consulta.restriccion(juego,atributo,valor)
